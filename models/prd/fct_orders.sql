b85a4cc6dfc71432a94fce1a867c58ebcf3b672d
Sérgio Miguel Sousa Castro Pereira <tpessoalsp@gmail.com>
2023-09-22 16:29:46 +0100

    Update fct_orders.sql
    Tivemos um erro ao construir a tabela, porque estava a apontar para uma tabela no teu ambiente, mas faltavam permissões para a utilizar. Desta forma, utilizamos o nosso snapshot.

--- models/prd/fct_orders.sql
+++ models/prd/fct_orders.sql
@@ -1,22 +1,18 @@
-
 {{
   config(
     materialized = 'table'
     )
 }}
 
-
 with valid_orders as (
 
 select OrderId, sum(SalePrice) as OrderTotalAmount, count(OrderItemId) as NumItems
-from `dbt-fundamentals-lvmartins.snapshots.snapshot_order_items`
+from {{ ref("snapshot_order_items") }}
 group by OrderId
-
 )
 
-
-
+  
 select oi.*, vo.OrderTotalAmount, vo.NumItems from 
-`snapshots.snapshot_order_items` oi
+{{ ref("snapshot_order_items") }} oi
 left join valid_orders vo ON oi.OrderId = vo.OrderId
 where dbt_valid_to is null
